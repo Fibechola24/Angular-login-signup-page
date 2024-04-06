4baf3a5d904b0aa7e4d7ae076f328376fc54f6ba
victor Okpanachi <okpanachivictor0@gmail.com>
2024-04-06 19:34:20 +0200

    used ngModule to bind the form input with the signup objects

--- src/app/components/user/user-login/user-login.component.ts
+++ src/app/components/user/user-login/user-login.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-login.component.html',
   styleUrl: './user-login.component.css'
 })
-export class UserLoginComponent {
+export class UserLoginComponent implements OnInit{
+// create an array to store the signup and also the objects
+//then bind the objects to a form using ngModule
+signupUsers: any[] = [];
+signupObj: any = {
+  username: '',
+  email: '',
+  password: '' 
+};
+loginObj: any = {
+  userName: '',
+  password: ''
+};
 
+  constructor() {}
+
+  ngOnInit(): void { 
+  }
+ 
 }
+
